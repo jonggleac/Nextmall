f7dc7d3f2ef0c4abb5e406e3987ab1608add9cd1
jonggleac <jonggleac@github.io>
2022-10-19 23:52:13 +0900

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,46 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { useContext } from 'react'
+import { Store } from '../utils/Store'
+export default function Layout({ title, children }) {
+  const { state } = useContext(Store)
+  const { cart } = state
+
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' - MyMall' : 'Mymall'}</title>
+        <meta name="description" content="Nextjs Ecommerce" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md bg-slate-200">
+            <Link href="/">
+              <a className="text-lg font-bold">MyMall</a>
+            </Link>
+            <div>
+              <Link href="/login">
+                <a className="p-2">Login</a>
+              </Link>
+              <Link href="/cart">
+                <a className="p-2">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </span>
+                  )}
+                </a>
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner bg-red-100">
+          <p>Copyright &copy; 2022 MyMall</p>
+        </footer>
+      </div>
+    </>
+  )
+}
