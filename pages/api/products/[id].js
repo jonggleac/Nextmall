559aef355529cd59f91fd29814f34fd445ab241f
jonggleac <jonggleac@github.io>
2022-11-30 19:08:44 +0900

    CreateOrderScreen

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -3,7 +3,7 @@ import db from '../../../utils/db'
 
 const handler = async (req, res) => {
   await db.connect()
-  const product = await Product.findByIdAndDelete(req.query.id)
+  const product = await Product.findById(req.query.id)
   await db.disconnect()
   res.send(product)
 }
